78a760e3d8706b24ec5e8a5beccb3515d9f4b228
Victor <cuevas0122@gmail.com>
2023-03-21 22:04:42 -0600

    primer commit

---
+++ SistemaDeGestionDeTutorias/Utilidades/MensajeUsuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SistemaDeGestionDeTutorias.Utilidades
+{
+    public static class MensajeUsuario
+    {
+        public static void MostrarGuardadoExitoso()
+        {
+            MessageBox.Show("Guardado Exitoso", "Guardado exitoso",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+        public static void MostrarErrorBD()
+        {
+            MessageBox.Show("Error En Base de datos", "Error En Base de datos", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static bool MostrarCofirmacionCancelar()
+        {
+            MessageBoxResult messageResult = MessageBox.Show("Perdera todo el progreso no guardado", "Confirmar cancelacion? ", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if(messageResult == MessageBoxResult.Yes)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SistemaDeGestionDeTutorias/Utilidades/Validador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeGestionDeTutorias.Utilidades
+{
+    public static class Validador
+    {
+        public static bool EsCorreoInstitucionalValido(string correo)
+        {
+            if (! (correo.Length == 0) ) { return false; }
+            if ( correo.Contains(" ") ) { return false; }
+            if (!correo.Contains("@")) { return false; }
+            if ( correo.StartsWith("@") ) { return false; }
+            
+
+            String[] emailParts = correo.Split('@');
+            if (emailParts[0].Length == 0) { return false; }
+            if (!emailParts[1].Contains('.')) { return false; }
+            if (!emailParts[1].EndsWith("profesores.uv.mx")) { return false; }
+            return true;
+        }
+        public static bool EsCorreoInstitucionalEstudianteValido(string correo)
+        {
+            if (!(correo.Length == 0)) { return false; }
+            if (correo.Contains(" ")) { return false; }
+            if (!correo.Contains("@")) { return false; }
+            if (correo.StartsWith("@")) { return false; }
+            
+
+            String[] emailParts = correo.Split('@');
+            if (emailParts[0].Length == 0) { return false; }
+            if (!emailParts[1].Contains('.')) { return false; }
+            if (!emailParts[1].EndsWith("estudiantes.uv.mx")) { return false; }
+            return true;
+        }
+        public static bool esApellidoValido(string apellido)
+        {
+            if (apellido.Length == 0) { return false; }
+            return true;
+        }
+        public static bool esPasswordValido(string password)
+        {
+            if (password.Length < 8) { return false; }
+            if (!password.Any(c => char.IsLower(c))) { return false; } 
+            if (!password.Any(c => char.IsUpper(c))) { return false; }  
+            if (!password.Any(c => char.IsNumber(c))) { return false; } 
+            if (!password.Any(c => char.IsPunctuation(c))) { return false; }  
+            return true;
+        }
+    }
+}
